fac7d074953c76f4eec965af39522aa24ae95c66
jaym2109 <jason@ontariopanelization.com>
2019-11-01 11:37:08 -0400

    custom error handling, mongoose middleware, mapquest api setup

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const errorHandler = require("./middleware/error");
 
 // Load config file
 dotenv.config({ path: "./config/config.env" });
@@ -25,6 +26,9 @@ if (process.env.NODE_ENV === "development") {
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
+
+//Error Handler middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
